3db99c940c8665b79147b99825f383508138abaf
정태영 <jty419@naver.com>
2022-12-27 17:10:12 +0900

    Feat: Separate partitions, GUI_Simple

---
+++ SimpleGUI/src/common/Constant.js
@@ -0,0 +1,18 @@
+export default {
+  //-------patient
+  GET_ALLPATIENTLIST: "getAllPatientList",
+  SET_ALLPATIENTLIST: "setAllPatientList",
+  GET_PATIENTNAME: "getPatientName",
+  SET_PATIENTNAME: "setPatientName",
+
+  GET_PATIENTSERIESLIST: "getPatientSeriesList",
+  SET_PATIENTSERIESLIST: "setPatientSeriesList",
+  
+  SET_BLOBIMGS: "setBlobImgs",
+  GET_BLOBIMGS: "getBlobImgs",
+  
+  GET_PATIENTRECORDLIST: "getPatientRecordList",
+  SET_PATIENTRECORDLIST: "setPatientRecordList",
+  GET_PATIENTRECORDFILENAMELIST: "getPatientRecordFilenameList",
+  SET_PATIENTRECORDFILENAMELIST: "setPatientRecordFilenameList",
+};
---
+++ SimpleGUI/src/store/index.js
@@ -0,0 +1,104 @@
+// import { login, findById } from "@/api/member.js";
+import drf from '@/api/drf'
+import Constant from "@/common/Constant.js";
+import { createStore } from 'vuex'
+import axios from "axios"
+
+export default createStore({
+  state: {
+    // [] 배열, {} 맵
+    allPatientList: [],
+
+    patientSeriesList: {},
+
+    patientRecordList: [],
+
+    patientRecordFilenameList: [],
+  },
+
+  getters: {
+    allPatientList: state => state.allPatientList,
+
+    patientSeriesList: state => state.patientSeriesList,
+
+    patientRecordList: state => state.patientRecordList,
+
+    patientRecordFilenameList: state => state.patientRecordFilenameList,
+  },
+
+  mutations: {
+    [Constant.SET_ALLPATIENTLIST]: (state, allPatientList) => state.allPatientList = allPatientList,
+
+    [Constant.SET_PATIENTSERIESLIST]: (state, patientSeriesList) => state.patientSeriesList = patientSeriesList,
+
+    [Constant.SET_PATIENTRECORDLIST]: (state, patientRecordList) => state.patientRecordList = patientRecordList,
+
+    [Constant.SET_PATIENTRECORDFILENAMELIST]: (state, patientRecordFilenameList) => state.patientRecordFilenameList = patientRecordFilenameList,
+  },
+
+  actions: {
+    [Constant.GET_ALLPATIENTLIST]({ commit }) {
+      axios({
+        url: drf.patient.allPatientList(),
+        method: 'get'
+      })
+      .then(res => {
+        // console.log(res.data.Result.Entities);
+        // (4) [{…}, {…}, {…}, {…}]
+        commit(Constant.SET_ALLPATIENTLIST, res.data.Result.Entities)
+      })
+      .catch(err => console.error(err))
+    },
+
+    [Constant.GET_PATIENTSERIESLIST]({commit}, uid) {
+      // uid.target.value = chartID
+      axios({
+        url: drf.patient.patientSeriesList(uid.target.value),
+        method: 'get'
+      })
+      .then(res => {
+        // res.data.Result.SeriesList.Entities = instanceUid list
+        // console.log(res.data.Result.SeriesList.Entities);
+        // {entity: Array(1), chartId: '20221013002'}
+        // 1.2.410.200062.2.1.20221222144825737.86.125615.843.799
+        const datas = {
+          entity: res.data.Result.SeriesList.Entities,
+          chartId: uid.target.value
+        }
+        // const newArr = datas.map(element => {
+        //   return element.UniqueID;
+        // });
+        // console.log(newArr);
+        commit(Constant.SET_PATIENTSERIESLIST, datas);
+      })
+      .catch(err => console.error(err))
+    },
+
+    [Constant.GET_PATIENTRECORDLIST]({ commit }, p_uid) {
+      axios({
+        url: drf.patient.patientRecordList(p_uid),
+        method: 'get'
+      })
+      .then(res => {
+        console.log(res.data.Result.Entities);
+        commit(Constant.SET_PATIENTRECORDLIST, res.data.Result.Entities)
+      })
+      .catch(err => console.error(err))
+    },
+
+    [Constant.GET_PATIENTRECORDFILENAMELIST]({ commit }, datas) {
+      axios({
+        url: drf.patient.patientRecordFilenameList(datas.pn, datas.ix),
+        method: 'get'
+      })
+      .then(res => {
+        console.log(res.data.patientRecordFilenameList)
+        commit(Constant.SET_PATIENTRECORDFILENAMELIST, res.data.patientRecordFilenameList)
+      })
+      .catch(err => console.error(err))
+    },
+  },
+
+  modules: {
+  }
+})
